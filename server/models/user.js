11e3b521c5c2e44faa80f0339d48f58bbee5df1b
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 18:20:07 +0300

    compare passwords in bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -29,6 +29,14 @@ userSchema.pre('save', function(next) {
     });
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) { return callback(err); }
+
+        callback(null, isMatch);
+    });
+}
+
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,23 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = {
+    usernameField: 'email'
+}
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+    // Verify email and password, call done with the user
+    // if it is the correct email and password
+    // otherwise, call done with false
+    User.findOne({ email: email}, function(err, user) {
+        if(err) { return done(err); }
+        if(!user) { return done(null, false); }
+
+        // compare passwords - is 'passwords' equal to user.password?
+    });
+});
 
 // Setup options for JWT strategy
 const jwtOptions = {
