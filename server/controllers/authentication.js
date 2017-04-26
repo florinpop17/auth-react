eec2f7d7664ea7b3776f284fc6928d21fb761695
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 17:23:00 +0300

    finished the auth.js

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -5,12 +5,24 @@ exports.signup = function(req, res, next) {
 
     // See if a user with the given email exists
     User.findOne({ email }, function(err, existingUser){
-        
+        if(err) { return next(err); }
+
+        // If a user with email does exist, return an error
+        if(existingUser) {
+            return res.status(422).send({ error: 'Email is in use' }); // 422 => unprocesable
+        }
+
+        // If a user with email does NOT exist, create and save user record
+        const user = new User({
+            email, password
+        });
+
+        // Save to database
+        user.save(function(err) {
+            if(err) { return next(err); }
+
+            // Respond to request indicating the user was created
+            res.json({ success: true });
+        });
     });
-
-    // If a user with email does exist, return an error
-
-    // If a user with email does NOT exist, create and save user record
-
-    // Respond to request indicating the user was created
 }
