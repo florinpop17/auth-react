50ade2313ce9c6402033698688d0e42e841448c4
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 20:22:39 +0300

    cli added

---
+++ cli/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+            </Route>
+        </Router>
+    </Provider>
+    , document.querySelector('.container'));
