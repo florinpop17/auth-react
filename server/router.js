793ee7e75a7877e9dda30a0257c865f5d35b30d7
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 18:07:19 +0300

    success passport authorization

--- server/router.js
+++ server/router.js
@@ -1,5 +1,12 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
+    app.get('/', requireAuth, function(req, res){
+        res.send({hi:'there'});
+    });
     app.post('/signup', Authentication.signup);
 }
---
+++ server/services/passport.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JWT strategy
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
+
+// Create JWT Strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    // See if the user ID in the payload exists in our database
+    // If it does, call 'done' with that user
+    // otherwise, call 'done' without a user object
+    User.findById(payload.sub, function(err, user){
+        if(err) { return done(err, false); }
+
+        if(user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
+
+// Tell passport to use this Strategy
+passport.use(jwtLogin);
