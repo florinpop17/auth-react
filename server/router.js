cdcdb7c86b79405ac4e0c42f1027d9902b47c9d8
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 16:55:01 +0300

    authentication controller

--- server/router.js
+++ server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
-    app.get('/', function(req, res, next){
-        res.send(['water', 'phone', 'paper'])
-    });
+    app.post('/signup', Authentication.signup);
 }
