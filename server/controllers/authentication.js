e414630d0b129adae7b1546eb3267e28989f1f19
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 17:46:42 +0300

    json web token (jwt)

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const { email, password } = req.body;
@@ -13,7 +20,7 @@ exports.signup = function(req, res, next) {
 
         // If a user with email does exist, return an error
         if(existingUser) {
-            return res.status(422).send({ error: 'Email is already in use!' }); // 422 => unprocesable
+            return res.status(422).send({ error: "Email is already in use!" }); // 422 => unprocesable
         }
 
         // If a user with email does NOT exist, create and save user record
@@ -27,7 +34,7 @@ exports.signup = function(req, res, next) {
             if(err) { return next(err); }
 
             // Respond to request indicating the user was created
-            res.json({ success: true });
+            res.json({ token: tokenForUser(user) });
         });
     });
 }
