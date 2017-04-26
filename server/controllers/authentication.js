5ea1600e14786cb242870d51ca20bdcfe1f946e7
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 17:01:09 +0300

    user.findone

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,16 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-    res.send({ success: 'true' });
+    const { email, password } = req.body;
+
+    // See if a user with the given email exists
+    User.findOne({ email }, function(err, existingUser){
+        
+    });
+
+    // If a user with email does exist, return an error
+
+    // If a user with email does NOT exist, create and save user record
+
+    // Respond to request indicating the user was created
 }
