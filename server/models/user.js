0794c40774d456d71d0da28fa115d389de1c8963
Florin Pop <popflorin1705@yahoo.com>
2017-04-26 17:33:48 +0300

    bcrypt comments

--- server/models/user.js
+++ server/models/user.js
@@ -9,19 +9,24 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre('save', function(next) {
+    // get access to the user model
     const user = this;
 
+    // generate a salt then run callback
     bcrypt.genSalt(10, function(err, salt) {
         if (err) { return next(err); }
 
+        // hash (encrypt) our password using the salt
         bcrypt.has(user.password, salt, null, function(err, hash) {
             if (err) { return next(err); }
 
+            // overwrite plain text password with encrypted password
             user.password = hash;
             next();
-        })
-    })
+        });
+    });
 });
 
 // Create the model class
